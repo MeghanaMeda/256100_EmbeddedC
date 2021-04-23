d5cc0442205545ae9d4dda4e34115a6b86859ba5
MeghanaMeda <36812612+MeghanaMeda@users.noreply.github.com>
2021-04-23 23:18:10 +0530

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,48 @@
+/*
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+int main(void)
+{
+    DDRB |= (1<<PB0);
+    DDRD &= ~(1<<PD0);
+    PORTD |= (1<<PD0);
+    DDRD &= ~(1<<PD1);
+    PORTD |= (1<<PD1);
+
+    while(1)
+    {
+        if((PIND&(1<<PD0)) && (PIND&(1<<PD1)))
+        {
+            PORTB |= (1<<PB0);
+            _delay_ms(20);
+        }
+        else if(!(PIND&(1<<PD0)) && (PIND&(1<<PD1)))
+        {
+            PORTB &= ~(1<<PB0);
+            _delay_ms(20);
+        }
+        else if((PIND&(1<<PD0)) && !(PIND&(1<<PD1)))
+        {
+            PORTB &= ~(1<<PB0);
+            _delay_ms(20);
+        }
+        else if(!(PIND&(1<<PD0)) && !(PIND&(1<<PD1)))
+        {
+            PORTB &= ~(1<<PB0);
+            _delay_ms(20);
+        }
+        else
+        {
+            PORTB &= ~(1<<PB0);
+            _delay_ms(20);
+        }
+
+    }
+
+    return 0;
+}
