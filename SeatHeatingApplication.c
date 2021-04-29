a5e836d3e10b063fc84a78a536ebcc38daeb19a9
MeghanaMeda <36812612+MeghanaMeda@users.noreply.github.com>
2021-04-29 13:20:53 +0530

    Add files via upload

--- SeatHeatingApplication.c
+++ SeatHeatingApplication.c
@@ -1,1 +1,30 @@
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
 
+int main(void)
+{
+    uint16_t temp;
+    
+    while(1)
+    {
+        if(LED()==1) //Check if both the switches are pressed
+        {
+           
+            TurnLED_ON();//Turn LED ON
+            temp=GetADC(); //Get the ADC value
+            PWM(temp); //PWM output based on temperature
+		    USARTWrite(temp); //To Serial monitor to print Temperature
+            
+
+        }
+        else  //in all other cases
+        {
+            TurnLED_OFF();//Turn LED OFF
+		    _delay_ms(200);
+        }
+
+    }
+    return 0;
+}
