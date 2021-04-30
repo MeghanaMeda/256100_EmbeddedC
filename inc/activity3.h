c4f079bafb9df7155ce4be160c8113bea669c984
MeghanaMeda <36812612+MeghanaMeda@users.noreply.github.com>
2021-04-30 22:21:49 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -17,7 +17,7 @@
 #define PWM_20_PERCENT (205) /**< Output PWM for 20% duty cycle */
 #define PWM_40_PERCENT (410) /**< Output PWM for 40% duty cycle */
 #define PWM_70_PERCENT (717) /**< Output PWM for 70% duty cycle */
-#define PWM_95_PERCENT (973) /**< Output PWM for 95% duty cycle */
+#define PWM_90_PERCENT (922) /**< Output PWM for 90% duty cycle */
 
 /**
  * Include files
